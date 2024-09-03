b782a604b1da4a0ba66fec85a3427df38534dad2
Stefan Zink <Stefan.G.Zink+gh@gmail.com>
2024-09-03 10:17:10 +0200

    Version running with testdata

---
+++ SymlinkPhotoFavoriten/FileObject.py
@@ -0,0 +1,179 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import string
+import logging
+import logging.config 
+import os
+import re
+import time,datetime
+
+import gi
+gi.require_version('GExiv2', '0.10')
+from gi.repository import GExiv2
+   
+
+    
+class FileObject:
+  ip = {}
+  # keys :
+  # ROOT-DIR ,ORIGINALS-DIR,ABS-FAVORITES-TGT-DIR,FAVORITES-TGT-DIR,FAVORITES-SRC-DIR,ABS-ORIGINALS-DIR,ABS-FAVORITES-SRC-DIR'}
+  absCopiesOrigDateiName = u""
+  fileBaseName    = u""
+  fileId = u""
+  absDateiNameOnOriginal = u""
+  copiesPathRelativeToRootDir = u"" 
+  copiesDirRelativeToRootDir = u""
+  copiesTgtDirRelativeToRootDir=u""
+  copiesLinkDepthToBaseDir = 0
+  dateiNameOnOriginalRelativeToRootDir = u""
+  directoryNameOnOriginalRelativeToRootDir = u""
+  exifDateTimeString = None
+  foundOriginal = False
+  findMethod = "no"
+   
+  def printOut(self):
+    output = "\r\nfileBaseName = " + self.fileBaseName + \
+             "\r\nabsCopiesOrigDateiName = " + self.absCopiesOrigDateiName + \
+             "\r\ncopiesPathRelativeToRootDir = " + self.copiesPathRelativeToRootDir + \
+             "\r\ncopiesDirRelativeToRootDir = " + self.copiesDirRelativeToRootDir + \
+             "\r\ncopiesTgtDirRelativeToRootDir = " + self.copiesTgtDirRelativeToRootDir + \
+             "\r\ncopiesLinkDepthToBaseDir = " + str(self.copiesLinkDepthToBaseDir) + \
+             "\r\nabsDateiNameOnOriginal = " + self.absDateiNameOnOriginal + \
+             "\r\ndateiNameOnOriginalRelativeToRootDir = " + self.dateiNameOnOriginalRelativeToRootDir + \
+             "\r\ndirectoryNameOnOriginalRelativeToRootDir = " + self.directoryNameOnOriginalRelativeToRootDir + \
+             "\r\nexifDateTimeString = " + str(self.exifDateTimeString) + \
+             "\r\nfindMethod = " + self.findMethod + \
+             "\r\nfoundOriginal = " + str(self.foundOriginal) + \
+             "\r\nfileId = " + self.fileId + \
+             "\r\n"
+    return output
+  
+
+  def getDateTimeStringFromExif(absDateiName):
+      retVal = None
+      try:
+          src_exif = GExiv2.Metadata(absDateiName)
+          exifDateTime = src_exif['Exif.Photo.DateTimeOriginal']
+          if exifDateTime is not None:
+              #Python 3
+              trantab = str.maketrans({'_':'', ' ':'', ':':''})
+              retVal = exifDateTime.translate(trantab)
+      except KeyError:
+          logging.debug("Image " + absDateiName + " does not have exiv date time")
+      except:
+          logging.debug("Fatal error " + absDateiName + " does not have exiv date time")
+      return retVal 
+
+  def initialize(self,inputParams,in_absCopiesOrigDateiName, originalFilesDict):
+    self.ip = inputParams
+    self.absCopiesOrigDateiName = in_absCopiesOrigDateiName
+    self.fileBaseName = os.path.basename(self.absCopiesOrigDateiName)
+    self.fileId = self.fileBaseName.rsplit('_',1)[0]
+    found = False 
+    self.exifDateTimeString = FileObject.getDateTimeStringFromExif(self.absCopiesOrigDateiName)
+    found = self.findFileInDirsExact(originalFilesDict)
+    # TODO handle mutiple hits
+    logging.debug("Found Exact Match for: "+ self.fileBaseName)
+    # if found == False:
+    #   found = self.findFileInDirsFuzzy(originalFilesDict)
+
+    self.copiesPathRelativeToRootDir=self.absCopiesOrigDateiName[self.ip["SRC-ROOT-DIR_LENGTH"]+1:]          
+    self.copiesDirRelativeToRootDir=os.path.dirname(self.copiesPathRelativeToRootDir)
+    lengthCopiesOrigDir= len(self.ip['FAVORITES-SRC-DIR'])
+    self.copiesLinkDepthToBaseDir=self.copiesTgtDirRelativeToRootDir.count(os.sep) # +1
+          
+    if found == True:  
+      self.dateiNameOnOriginalRelativeToRootDir=self.absDateiNameOnOriginal[self.ip["SRC-ROOT-DIR_LENGTH"]+1:]
+      self.directoryNameOnOriginalRelativeToRootDir=os.path.dirname(self.dateiNameOnOriginalRelativeToRootDir)
+      self.copiesTgtDirRelativeToRootDir=os.path.join(self.ip['FAVORITES-TGT-DIR'],self.copiesDirRelativeToRootDir[lengthCopiesOrigDir+1:] )
+    else:
+      self.copiesTgtDirRelativeToRootDir=os.path.join(self.ip['NOTFOUND_FILES_TGT_DIR'],self.copiesDirRelativeToRootDir[lengthCopiesOrigDir+1:] )
+          
+      
+
+  def findFileInDirsExact(self,originalFilesDict):
+      if self.fileBaseName in originalFilesDict:
+          self.absDateiNameOnOriginal = originalFilesDict[self.fileBaseName]['absOrigFileName']
+          self.foundOriginal = True
+          self.findMethod="Exact"
+      else:
+            self.foundOriginal = False   
+      return self.foundOriginal        
+ 
+
+ 
+#   def findFileInDirsFuzzy(self,originalFilesDict):
+#       for curOrigFileName in originalFilesDict:
+#           match = re.search(self.fileBaseName,curOrigFileName,re.IGNORECASE)
+#           if match != None:
+#             self.absDateiNameOnOriginal = originalFilesDict[curOrigFileName]['absOrigFileName']
+#             self.foundOriginal = True
+#             self.findMethod="CaseInsensitive"
+#             break
+#           else:
+#             curOrigFileId = curOrigFileName.rsplit('_',1)[0]  
+#             # special cases
+#             #20091108 11:18:38_TaufeVonValentin.jpg
+#             #20100215 092113_Fasching im Kindergarten.jpg
+#             # IMG_0891=IMG_2210
+#             translation_table = dict.fromkeys(map(ord, ': _'), None)
+#             curOrigFileIdNormalized = curOrigFileId.translate(translation_table)
+#             selfFileIdNormalized = self.fileId.translate(translation_table)
+#             match = re.match('\A(\d)+',selfFileIdNormalized,re.UNICODE)
+#             if match:
+#                 selfFileIdNormalized = match.group(0)
+#             else:
+#                 # orig Id does not correspond to a date
+#                 origExifDateTimeString = originalFilesDict[curOrigFileName]['exifDateTimeString']
+#                 logging.debug("exiv no id search for ) "+ str(self.fileBaseName) +" == " +curOrigFileName )
+#                 if (self.exifDateTimeString == curOrigFileIdNormalized) and (selfFileIdNormalized != ""):
+#                    self.absDateiNameOnOriginal = originalFilesDict[curOrigFileName]['absOrigFileName']
+#                    self.findMethod="ExivDate NoID"
+#                    self.foundOriginal = True
+#                    break
+#             logging.debug("fuzzy method 1: "+ selfFileIdNormalized +" == " +curOrigFileIdNormalized )
+#             if match and (selfFileIdNormalized == curOrigFileIdNormalized) and (selfFileIdNormalized != ""):
+#               self.absDateiNameOnOriginal = originalFilesDict[curOrigFileName]['absOrigFileName']
+#               self.findMethod="Fuzzy 1"
+#               self.foundOriginal = True
+#               break
+#               #20100320100413FamilieZinkbeimPhotograph.jpg
+# #                   logging.debug("fuzzy method 2: "+ selfFileIdNormalized +" == " +curOrigFileIdNormalized )
+# #                   if (selfFileIdNormalized == curOrigFileIdNormalized) and (selfFileIdNormalized != ""):
+# #                       self.absDateiNameOnOriginal = originalFilesDict[curOrigFileName]
+# #                       self.foundOriginal = True
+# #                       self.findMethod="Fuzzy 2"
+# #                       break
+#             else:
+#                #20050907_123010_RadtourWildbadKreuthStrandRottachEgern.jpg ==  20050907_20RadtourWildbadKreuthStrandRottachEgern.JPG  
+#                #nur mit exiv date zu lösen benutze gexiv library
+#                logging.debug("fuzzy method 3 (exiv) "+ str(self.exifDateTimeString) +" == " +curOrigFileIdNormalized )
+#                if (self.exifDateTimeString == curOrigFileIdNormalized) and (selfFileIdNormalized != ""):
+#                    self.absDateiNameOnOriginal = originalFilesDict[curOrigFileName]['absOrigFileName']
+#                    self.findMethod="ExivDate"
+#                    self.foundOriginal = True
+#                    break
+#       return self.foundOriginal        
+
+ 
+ 
+  def comparePicturesByExifDate(self,curOrigFullFileName):
+     try:
+         src_exif = GExiv2.Metadata(self.absCopiesOrigDateiName)
+         src_ExifDate = src_exif.get_date_time().strftime('%Y%m%d%H%M%S')
+         #logging.DEBUG("src-exifTimeStamp for " + self.fileBaseName + " is " + src_ExifDate)
+         tgt_exif = GExiv2.Metadata(curOrigFullFileName)
+         tgt_ExifDate = tgt_exif.get_date_time().strftime('%Y%m%d%H%M%S')
+         #logging.DEBUG("tgt-exifTimeStamp for " + os.path.basename(curOrigFullFileName) + " is " +tgt_ExifDate) )
+         if src_ExifDate == tgt_ExifDate:
+             return True
+         else:
+             return False
+     except:
+        logging.debug("Image does not have exiv date time")
+        return False
+      
+ ################################################################################################      
+#end class      
---
+++ SymlinkPhotoFavoriten/SymlinkPhotoFavoriten.py
@@ -0,0 +1,216 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+description = """This program handles handles directories with favorite Photos and creates a directory with absolute links to the original file
+if found on the originals dir  
+
+SymlinkFavoriten.py <option> [<xml_configfile>]
+
+"""
+
+import os
+import shutil
+import glob
+import sys
+import re
+import string
+import logging
+import logging.config 
+# from xml import dom
+from xml.dom import minidom
+from xml.dom import Node
+import codecs
+import json
+import yaml
+import shutil
+from FileObject import FileObject
+import time,datetime
+#efrom functions import initLogger
+# import scriptutil as SU
+    
+
+def initLogger(config):
+    handler = logging.StreamHandler(sys.stdout)
+    frm = logging.Formatter("%(asctime)s [%(levelname)-5s]: %(message)s", "%Y%m%d %H:%M:%S")
+    handler.setFormatter(frm)
+    logger = logging.getLogger()
+    logger.addHandler(handler)
+    if config["loglevel"] == "DEBUG":
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+    return logger
+
+
+###########################################################################
+def extendInputParams(inputParams):
+  inputParams["ABS-FAVORITES-SRC-DIR"] = os.path.join(inputParams["SRC-ROOT-DIR"], inputParams["FAVORITES-SRC-DIR"])
+  inputParams["ABS-FAVORITES-TGT-DIR"] = os.path.join(inputParams["TGT-ROOT-DIR"], inputParams["FAVORITES-TGT-DIR"])
+  inputParams["SRC-ROOT-DIR_LENGTH"] = len(inputParams["SRC-ROOT-DIR"])
+  inputParams["TGT-ROOT-DIR_LENGTH"] = len(inputParams["TGT-ROOT-DIR"])
+  logging.debug("extended inputParams = \n%s" % inputParams) 
+  return inputParams
+
+###########################################################################
+def createFileObjectsList(inputParams):
+  fileObjects = []
+  notFoundFileObjects = []
+  originalFilesDict = {}
+  # create searchPattern from extensions
+  searchPattern = r"("
+  for i in range (0,len(config['searchExtension'])):
+      if i == 0:
+          searchPattern = searchPattern + "." + config['searchExtension'][i] 
+      else:
+          searchPattern = searchPattern + "|" + "." + config['searchExtension'][i]
+  searchPattern = searchPattern + ")"    
+  logging.info("searchPattern :" + searchPattern )
+      
+  # create dictionary with original files to improve performance
+  for relDirName in inputParams["ORIGINALS-DIRS"]:
+      dirName = os.path.join(inputParams["SRC-ROOT-DIR"], relDirName)
+      logging.debug(" dirName = " + dirName)
+      for dir, dirList, fileList in os.walk (dirName):
+  #      logging.debug(" dirList = " + str(dirList) )
+  #      logging.debug(" fileList = " + str(fileList))
+        for file in fileList:
+            result = re.search(searchPattern, file, re.IGNORECASE)
+            if result != None:
+                origFileDict = {}
+                origFileDict['absOrigFileName']=os.path.join(dir, file)
+                origFileDict['exifDateTimeString']=FileObject.getDateTimeStringFromExif(os.path.join(dir, file))
+                originalFilesDict[os.path.basename(file)] = origFileDict
+  #iterate over source files 
+  for verz, verzList, dateiListe in os.walk (inputParams["ABS-FAVORITES-SRC-DIR"]):
+      logging.debug(" verzList = " + str(verzList))
+      logging.debug(" dateiListe = " + str(dateiListe))
+      for datei in dateiListe:
+          resultRE2 = re.search(searchPattern, datei, re.IGNORECASE)
+          if resultRE2 != None:
+              absCopiesOrigDateiName = os.path.join(verz, datei)
+              logging.debug(" absCopiesOrigDateiName = " + str(absCopiesOrigDateiName))
+              newFile = FileObject()
+              FileObject.initialize(newFile, inputParams, absCopiesOrigDateiName,originalFilesDict)
+              logging.debug(FileObject.printOut(newFile))
+              if newFile.foundOriginal == False:
+                notFoundFileObjects.append(newFile)
+                logging.info(newFile.findMethod.rjust(16) +" match found for: " + str(absCopiesOrigDateiName))
+              else:
+                fileObjects.append(newFile) 
+                logging.info(newFile.findMethod.rjust(16) + " match found for: " + str(absCopiesOrigDateiName))
+#           else:     
+#                 logging.info("no".rjust(16) + " match found for: " + str(absCopiesOrigDateiName))
+  return (fileObjects, notFoundFileObjects)
+
+###########################################################################
+def processFileObjects(fileObjects,inputParams):
+  outFilePath = os.path.join(inputParams['TGT-ROOT-DIR'], inputParams['FAVORITES-TGT-DIR'])
+  os.makedirs(outFilePath,exist_ok=True)  # ,'0775')
+  with open(os.path.join(outFilePath,"FoundFiles.txt"), 'w') as outfile:
+      for fileObject in fileObjects:
+          try:
+              newTgtDir = os.path.join(inputParams['TGT-ROOT-DIR'], fileObject.copiesTgtDirRelativeToRootDir)
+              if  not os.path.exists(newTgtDir):
+                  logging.debug("calling   os.makedirs(" + newTgtDir + ",'0775')")
+                  if inputParams["SIMULATE"] == False: 
+                      os.makedirs(newTgtDir)  # ,'0775')
+                
+              logging.debug("calling os.chdir(" + newTgtDir + ")")
+              if inputParams["SIMULATE"] == False: 
+                os.chdir(newTgtDir)
+              
+              #newRelLink = os.path.join("../"*fileObject.copiesLinkDepthToBaseDir, fileObject.dateiNameOnOriginalRelativeToRootDir)
+              #logging.debug("checking os.symlink(" + newRelLink + "," + fileObject.fileBaseName + ")")
+              newAbsLink = os.path.join(fileObject.ip['TGT-ROOT-DIR'], fileObject.dateiNameOnOriginalRelativeToRootDir)
+              logging.debug("checking os.symlink(" + newAbsLink + "," + fileObject.fileBaseName + ")")
+              if  not os.path.exists(fileObject.fileBaseName):
+                #logging.debug("calling os.symlink(" + newRelLink + "," + fileObject.fileBaseName + ")")
+                logging.debug("calling os.link(" + newAbsLink + "," + fileObject.fileBaseName + ")")
+                if inputParams["SIMULATE"] == False: 
+                  # os.symlink(newRelLink,fileObject.fileBaseName)
+                  if config['linkType'] == 'hard':
+                      os.link(newAbsLink, fileObject.fileBaseName)
+                  else:
+                      os.symlink(newAbsLink, fileObject.fileBaseName)  
+                      
+          except (OSError,IOError):
+              logging.error("error in processing " + fileObject.printOut())
+              logging.error("continuing .... ")
+              
+          
+          outLine = fileObject.findMethod.rjust(16) +":" + fileObject.fileBaseName.rjust(60) + " == " + os.path.basename(fileObject.absDateiNameOnOriginal) + " ## " + fileObject.absCopiesOrigDateiName + " == " + fileObject.absDateiNameOnOriginal 
+          logging.debug("outLine=" + outLine)
+          outfile.write(outLine + '\n')
+    
+############################################################################
+def writeNotFoundFilesToFile(notFoundFileObjects):
+    outFileName = os.path.join(inputParams['SRC-ROOT-DIR'], inputParams['FAVORITES-SRC-DIR'], "notFoundFiles.txt")
+#    with open(outFileName, 'w',encoding='utf-8') as outfile:
+    with open(outFileName, 'w') as outfile:
+        for curNotFoundFileObj in notFoundFileObjects:
+            outLine = curNotFoundFileObj.absCopiesOrigDateiName
+            logging.info("outLine=" + outLine)
+            outfile.write(outLine + '\n')
+    
+
+#############################################################################################
+def processNotFoundFiles(notFoundFileObjects):
+    for fileObject in notFoundFileObjects:
+        newTgtDir = os.path.join(fileObject.ip['SRC-ROOT-DIR'], fileObject.copiesTgtDirRelativeToRootDir)
+        logging.debug("calling   os.makedirs(" + newTgtDir + ",'0775')")
+        if inputParams["SIMULATE"] == False: 
+            os.makedirs(newTgtDir,exist_ok=True)  # ,'0775')
+        if  not os.path.exists(os.path.join(newTgtDir, fileObject.fileBaseName)):
+            logging.debug("checking shutil.copy(" + fileObject.absCopiesOrigDateiName + "," + newTgtDir)
+            if inputParams["SIMULATE"] == False: 
+                shutil.copy(fileObject.absCopiesOrigDateiName, newTgtDir)
+    writeNotFoundFilesToFile(notFoundFileObjects)       
+  
+
+############################################################################
+# main starts here
+
+if len(sys.argv) == 1 :
+    print(description)
+    print(sys.argv[0] + "<configfile>")
+    configFileName = 'config.yaml'
+else:
+    configFileName = sys.argv[1]
+
+
+with open(configFileName, 'r',encoding='utf-8') as cfgfile:
+    config = yaml.safe_load(cfgfile)
+logger = initLogger(config)
+
+#yamlFileName = 'SearchAndCopyFileFromOriginalDir_Photos.yaml'
+#with open(yamlFileName, 'w', encoding='utf-8') as outFile:
+#   outFile.write(yaml.dump(config))
+#exit(0)       
+
+configuration = config["configuration"]  
+inputParams = config[configuration]
+inputParams = extendInputParams(inputParams)
+
+begin = datetime.datetime.now()
+beginFormatted = begin.strftime('%Y-%m-%d %H:%M:%S')
+logging.info("starting processing at " + beginFormatted)
+
+(fileObjects, notFoundFileObjects) = createFileObjectsList(inputParams)
+processFileObjects(fileObjects,inputParams)
+processNotFoundFiles(notFoundFileObjects)
+#time.sleep(5)
+
+end = datetime.datetime.now()
+endFormatted = end.strftime('%Y-%m-%d %H:%M:%S')
+outFilePath = os.path.join(inputParams['TGT-ROOT-DIR'], inputParams['FAVORITES-TGT-DIR'])
+with open(os.path.join(outFilePath,"FoundFiles.txt"), 'a') as outfile:
+    logging.info("end processing at " + endFormatted)
+    outline = "processing took: " + str(end - begin)
+    logging.info(outline)
+    outfile.write(outline + '\n')
+    outline = ("Number of matched files %d" % len(fileObjects) )
+    logging.info(outline)
+    outfile.write(outline + '\n')
+    outline = ("Number of unmatched files %d" % len(notFoundFileObjects) )
+    logging.info(outline)
+    outfile.write(outline + '\n')
